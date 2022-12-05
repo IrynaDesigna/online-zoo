526b6a04122d9422ef689cebbbd80db3f7007320
IrynaDesigna <irynadesigna@gmail.com>
2022-12-04 17:55:10 -0700

    feat: add styles

---
+++ pages/main/script.js
@@ -0,0 +1,17 @@
+// ;(function () {
+//   "use strict";
+//
+// const pets = document.getElementsByClassName('pet');
+//
+// console.log(pets);
+//
+// if (window.innerWidth >= 1000) {
+//   for (let pet in pets) {
+//     pets[6].style.display = "none";
+//     pets[7].style.display = "none";
+//
+//   }
+// }
+//
+//
+// })();
